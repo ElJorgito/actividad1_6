2d899fe76ef11395be0467a2cac4495431e590ad
Jorge <11655449@alu.murciaeduca.es>
2025-10-25 16:46:18 +0200

    Commit 2

--- src/main/java/es/cifpcarlos3/actividad1_6/GestorCanciones.java
+++ src/main/java/es/cifpcarlos3/actividad1_6/GestorCanciones.java
@@ -2,6 +2,11 @@ package es.cifpcarlos3.actividad1_6;
 
 
 import es.cifpcarlos3.actividad1_6.vo.Cancion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,5 +20,22 @@ public class GestorCanciones {
         List<Cancion> lista = new ArrayList<>();
         int lineasLeidas = 0;
         int lineasIgnoradas = 0;
+
+        try (var br = Files.newBufferedReader(txt, StandardCharsets.UTF_8)){
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                lineasLeidas++;
+
+                String[] partes = linea.split(",");
+                if (partes.length != 5) {
+                    lineasIgnoradas++;
+                    continue;
+                }
+
+
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo " + e.getMessage());
+        }
     }
 }
--- src/main/java/es/cifpcarlos3/actividad1_6/vo/Cancion.java
+++ src/main/java/es/cifpcarlos3/actividad1_6/vo/Cancion.java
@@ -1,4 +1,15 @@
 package es.cifpcarlos3.actividad1_6.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
 public class Cancion {
+    private int anio;
+    private String titulo;
+    private String artista;
+    private String duracion;
+    private boolean esEspanola;
 }
