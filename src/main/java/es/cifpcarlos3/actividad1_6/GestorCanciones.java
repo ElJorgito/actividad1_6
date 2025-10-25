bc77ebbd9f49e8d45cf642207b0777822844d787
Jorge <11655449@alu.murciaeduca.es>
2025-10-25 15:30:20 +0200

    Commit 1

--- src/main/java/es/cifpcarlos3/actividad1_6/GestorCanciones.java
+++ src/main/java/es/cifpcarlos3/actividad1_6/GestorCanciones.java
@@ -1,8 +1,19 @@
 package es.cifpcarlos3.actividad1_6;
 
 
+import es.cifpcarlos3.actividad1_6.vo.Cancion;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorCanciones {
     public static void main(String[] args) {
-        
+        Path dir_base = Path.of("src","main","java","es","cifpcarlos3","actividad1_6");
+        Path txt = dir_base.resolve("canciones.txt");
+        Path json = dir_base.resolve("canciones.json");
+
+        List<Cancion> lista = new ArrayList<>();
+        int lineasLeidas = 0;
+        int lineasIgnoradas = 0;
     }
 }
